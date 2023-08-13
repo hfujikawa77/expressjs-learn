4d7f5d3c458d1f836657fe1177a58a7b9c4e1c3e
hideyuki.fujikawa <hideyuki.fujikawa@accenture.com>
2023-08-13 23:48:34 +0900

    チュートリアルのルーティングと静的ファイルを完了

--- myapp/README.md
+++ myapp/README.md
@@ -1,5 +1,14 @@
-
-Expressのチュートリアル、インストール～Hello Worldまでを実施
+## このプロジェクトは
+Expressのチュートリアルを実施。下記項目を完了
 
 https://expressjs.com/en/starter/installing.html  
 https://expressjs.com/en/starter/hello-world.html
+https://expressjs.com/en/starter/basic-routing.html
+https://expressjs.com/ja/starter/static-files.html
+
+## 実行方法
+下記コマンドで起動
+```
+node app.js
+```
+ブラウザで http://localhost:3000/ を開く
--- myapp/app.js
+++ myapp/app.js
@@ -6,6 +6,23 @@ app.get('/', (req, res) =>{
     res.send('Hello Express world!')
 })
 
+// post request
+app.post('/', (req,res) =>{
+    res.send('Got a POST request')
+})
+
+app.put('/user', function(req,res){
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req,res) =>{
+    res.send('Got a DELETE request at /user')
+})
+
+// 静的ファイルの提供。これでpublicフォルダ内のファイルをロードできる
+app.use('/static', express.static('public'))
+
 app.listen(port, ()=>{
     console.log(`Example app listening on port${port}`)
 })
+
